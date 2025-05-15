80f9867d87cbbb74291c0310e1cbc99ad0be9b99
Ninoh-FOX <ninohfox@gmail.com>
2025-05-15 16:13:43 +0200

    restore stock cpu mode

--- App/n64/launch.sh
+++ App/n64/launch.sh
@@ -18,3 +18,10 @@ keykill mupen64plus
 ./mupen64plus "$1" 2>&1 | tee $HOME/log.txt
 
 sync
+
+echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo 1008000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1 > /sys/devices/system/cpu/cpu0/online
+echo 1 > /sys/devices/system/cpu/cpu1/online
+echo 0 > /sys/devices/system/cpu/cpu3/online
+echo 0 > /sys/devices/system/cpu/cpu2/online
