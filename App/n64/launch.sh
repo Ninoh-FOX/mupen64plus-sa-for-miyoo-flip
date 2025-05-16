11ec021f0994b21c75cc4f23a05253d89cf689b0
ninoh-fox <ninohfox@gmail.com>
2025-05-16 09:18:56 +0200

    fixed close the emulator with select+start

--- App/n64/launch.sh
+++ App/n64/launch.sh
@@ -5,6 +5,7 @@ APPDIR="$(dirname "$0")"
 HOME="$APPDIR"
 
 cd $HOME
+PATH="/mnt/SDCARD/miyoo355/bin:$PATH"
 LD_LIBRARY_PATH="$HOME/lib:$LD_LIBRARY_PATH"
 
 echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -14,14 +15,16 @@ echo 1 > /sys/devices/system/cpu/cpu1/online
 echo 1 > /sys/devices/system/cpu/cpu3/online
 echo 1 > /sys/devices/system/cpu/cpu2/online
 
-keykill mupen64plus
-./mupen64plus "$1" 2>&1 | tee $HOME/log.txt
+gptokeyb -k "mupen64plus" &
+
+./mupen64plus "$1"
 
 sync
+pkill -9 gptokeyb
 
 echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 1008000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1 > /sys/devices/system/cpu/cpu0/online
 echo 1 > /sys/devices/system/cpu/cpu1/online
-echo 0 > /sys/devices/system/cpu/cpu3/online
 echo 0 > /sys/devices/system/cpu/cpu2/online
+echo 0 > /sys/devices/system/cpu/cpu3/online
